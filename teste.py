8c861e01e33f1c55b2e3377026e8b1be0d819b37
Eder Kapisch <eder.kapisch@engenharia.ufjf.br>
2023-08-17 18:13:46 -0300

    feat: pasta de funções

--- teste.py
+++ teste.py
@@ -1,27 +1,9 @@
 ## FUNCAO PLOTAR SENO E COSSENO
-import numpy as np
+from utils.funcs import plotar_seno_e_cosseno, plotar_exponencial_decrescente, plotar_janela_retangular
 import matplotlib.pyplot as plt
-
-def plotar_seno_e_cosseno():
-    x = np.linspace(-2*np.pi, 2*np.pi, 100)
-    y = np.sin(x)
-    z = np.cos(x)
-    plt.plot(x, y, label='seno')
-    plt.plot(x, z, label='cosseno')
-    plt.grid()
-    plt.legend()
-    plt.show()
-
-plotar_seno_e_cosseno()
-# plotar uma exponencial decrescente
-def plotar_exponencial_decrescente():
-    x = np.linspace(-2, 2, 100)
-    y = np.exp(-x)
-    plt.plot(x, y)
-    plt.grid()
-    plt.show()
     
+plotar_seno_e_cosseno()    
 plotar_exponencial_decrescente()
-
-
+plotar_janela_retangular()
+plt.show()
 
